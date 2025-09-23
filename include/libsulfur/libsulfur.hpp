2ba615bc6e3e299d7e45274368a81cdc1bc9fe1a
adoerr <0xad@gmx.net>
2025-09-23 08:26:18 +0200

    project setup
    Signed-off-by: adoerr <0xad@gmx.net>

---
+++ include/libsulfur/libsulfur.hpp
@@ -0,0 +1,6 @@
+
+#pragma once
+
+namespace sulfur {
+    void hello();
+}
---
+++ src/libsulfur.cpp
@@ -0,0 +1,7 @@
+import std
+
+#include <lubsulfur/ibsulfur.hpp>
+
+void libsulfur_hello() {
+    println("Hello from libsulfur!")
+}
