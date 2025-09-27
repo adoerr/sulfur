cd9f48c21d8c98e046712f74027ca8acce69d947
adoerr <0xad@gmx.net>
2025-09-27 18:26:18 +0200

    add class process
    Signed-off-by: adoerr <0xad@gmx.net>

---
+++ include/libsulfur/process.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <filesystem>
+#include <memory>
+#include <sys/types.h>
+
+namespace sulfur {
+    enum class process_state {
+        running,
+        stopped,
+        exited,
+        terminated
+    };
+
+    class process {
+    public:
+        ~process();
+
+        static std::unique_ptr<process> launch(std::filesystem::path path);
+        static std::unique_ptr<process> attach(pid_t pid);
+
+        void resume();
+        void wait_on_signal();
+        [[nodiscard]] pid_t pid() const { return pid_; }
+        [[nodiscard]] process_state state() const { return state_; }
+
+        // `process` is neither copyable nor movable
+        process() = delete;
+        process(const process&) = delete;
+        process& operator=(const process&) = delete;
+
+    private:
+        process(const pid_t pid, const bool terminate_on_end) : pid_(pid), terminate_on_end(terminate_on_end) {};
+
+        pid_t pid_{0};
+        bool terminate_on_end{true};
+        process_state state_{process_state::stopped};
+    };
+} // namespace sulfur
