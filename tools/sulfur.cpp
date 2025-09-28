5f35a555c303eb2d51ef2053eb6d4c42346170f6
adoerr <0xad@gmx.net>
2025-09-28 15:35:16 +0200

    refactor sulfur tool
    Signed-off-by: adoerr <0xad@gmx.net>

--- tools/sulfur.cpp
+++ tools/sulfur.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <unistd.h>
 #include <string_view>
-#include <sys/ptrace.h>
 #include <sys/wait.h>
 #include <editline/readline.h>
 #include <string>
@@ -9,73 +7,46 @@
 #include <algorithm>
 #include <sstream>
 #include <ranges>
+#include <string.h>
+#include <libsulfur/process.hpp>
+#include <libsulfur/error.hpp>
 
 namespace {
     /**
-      * Attaches to a process or forks a new process for tracing.
-      *
-      * @param argc The number of command-line arguments.
-      * @param argv The array of command-line arguments.
-      *             - If `-p <pid>` is provided, attaches to the process with the given PID.
-      *             - Otherwise, forks a new process and executes the specified program.
-      *
-      * @return The PID of the attached or forked process, or -1 on failure.
-    */
-    pid_t attach(const int argc, const char** argv) {
-        pid_t pid{0};
-
+     * Attaches to an existing process or launches a new one based on command-line arguments.
+     *
+     * @param argc The number of command-line arguments.
+     * @param argv The array of command-line argument strings.
+     * @return A `std::unique_ptr<sulfur::process>` representing the attached or launched process.
+     *
+     * If the arguments indicate attaching to an existing process (using `-p <pid>`), it attaches to that process.
+     * Otherwise, it launches a new process using the provided program path.
+     */
+    std::unique_ptr<sulfur::process> attach(const int argc, const char** argv) {
+        // attach to an existing process using PID
         if (argc == 3 && argv[1] == std::string_view{"-p"}) {
+            pid_t pid{0};
             pid = std::stoi(argv[2]);
-
-            if (pid <= 0) {
-                std::cerr << "Invalid pid" << std::endl;
-                return -1;
-            }
-
-            if (ptrace(PTRACE_ATTACH, pid, nullptr, nullptr) < 0) {
-                std::perror("Attach failed");
-                return -1;
-            }
-        }
-        else {
-            if ((pid = fork() < 0)) {
-                std::perror("Fork failed");
-                return -1;
-            }
-
-            // if we are the child process
-            if (pid == 0) {
-                // enable process tracing
-                if (ptrace(PTRACE_TRACEME, 0, nullptr, nullptr) < 0) {
-                    std::perror("Trace me failed");
-                    return -1;
-                }
-
-                if (const char* path = argv[1]; execlp(path, path, nullptr) < 0) {
-                    std::perror("Exec failed");
-                    return -1;
-                }
-            }
+            return sulfur::process::attach(pid);
         }
 
-        return pid;
+        // launch a new process using program path
+        const char* path = argv[1];
+        return sulfur::process::launch(path);
     }
 
     /**
-     * Splits a command into tokens based on a specified delimiter.
+     * Splits a command string into individual tokens based on spaces.
      *
-     * @param cmd The input command to be split.
-     * @param delim The character used as the delimiter to split the string.
-     * @return A `std::vector<std::string>` containing the tokens extracted from the input string.
-     *
-     * Split the input string `cmd` into tokens using the specified `delim` character. Tokens are stored in a vector and returned.
+     * @param cmd The command string to split.
+     * @return A vector of tokens extracted from the command string.
      */
-    std::vector<std::string> split(const std::string_view cmd, char delim) {
+    std::vector<std::string> split(const std::string_view cmd) {
         std::vector<std::string> tokens{};
         std::stringstream ss{std::string{cmd}};
         std::string item;
 
-        while (std::getline(ss, item, delim)) {
+        while (std::getline(ss, item, ' ')) {
             tokens.push_back(item);
         }
 
@@ -87,32 +58,66 @@ namespace {
             std::ranges::equal(str, of | std::views::take(str.size()));
     }
 
-    void resume(const pid_t pid) {
-        if (ptrace(PTRACE_CONT, pid, nullptr, nullptr) < 0) {
-            std::cerr << "Failed to resume process" << std::endl;
-            std::exit(-1);
+    void print_stop_reason(const sulfur::process& process, const sulfur::stop_reason& reason) {
+        std::cout << "Process " << process.pid() << " ";
+
+        switch (reason.reason) {
+        case sulfur::process_state::exited:
+            std::cout << "exited with code " << static_cast<int>(reason.info);
+            break;
+        case sulfur::process_state::stopped:
+            std::cout << "stopped with signal " << sigabbrev_np(reason.info);
+            break;
+        case sulfur::process_state::terminated:
+            std::cout << "terminated with signal " << sigabbrev_np(reason.info);
+            break;
+        default:
+            std::cout << "in unknown state";
+            break;
         }
+
+        std::cout << std::endl;
     }
 
-    void wait_on_signal(const pid_t pid) {
-        int status{0};
-
-        if (constexpr int options{0}; waitpid(pid, &status, options) < 0) {
-            std::perror("waitpid failed");
-            std::exit(-1);
-        }
-    }
-
-    void run_command(const pid_t pid, const std::string_view line) {
-        const auto args = split(line, ' ');
-        const auto& command = args[0];
+    void run_command(const std::unique_ptr<sulfur::process>& process, const std::string_view line) {
+        const auto args = split(line);
+        const auto command = args[0];
 
         if (is_prefix(command, "continue")) {
-            resume(pid);
-            wait_on_signal(pid);
+            process->resume();
+            const auto reason = process->wait_on_signal();
+            print_stop_reason(*process, reason);
         }
         else {
             std::cerr << "Unknown command: " << command << std::endl;
+        }
+    }
+
+    void command_loop(const std::unique_ptr<sulfur::process>& process) {
+        char* line = nullptr;
+        while ((line = readline("sulfur> ")) != nullptr) {
+            std::string line_str;
+
+            if (line == std::string_view("")) {
+                free(line);
+                if (history_length > 0) {
+                    line_str = history_get(history_length - 1)->line;
+                }
+            }
+            else {
+                line_str = line;
+                add_history(line);
+                free(line);
+            }
+
+            if (!line_str.empty()) {
+                try {
+                    run_command(process, line_str);
+                }
+                catch (const sulfur::error& e) {
+                    std::cerr << e.what() << std::endl;
+                }
+            }
         }
     }
 }
@@ -123,36 +128,11 @@ int main(const int argc, const char** argv) {
         return -1;
     }
 
-    const pid_t pid = attach(argc, argv);
-
-    // wait for the inferior to stop after attach
-    int status{0};
-
-    if (constexpr int options{0}; waitpid(pid, &status, options) < 0) {
-        std::perror("Waitpid failed");
-        return -1;
+    try {
+        const auto process = attach(argc, argv);
+        command_loop(process);
     }
-
-    char* line{nullptr};
-
-    while ((line = readline("sulfur> ")) != nullptr) {
-        std::string line_str;
-
-        // an empty line means to repeat the last command
-        if (line == std::string_view("")) {
-            free(line);
-            if (history_length > 0) {
-                line_str = history_list()[history_length - 1]->line;
-            }
-        }
-        else {
-            line_str = line;
-            add_history(line);
-            free(line);
-        }
-
-        if (!line_str.empty()) {
-            run_command(pid, line_str);
-        }
+    catch (const sulfur::error& e) {
+        std::cerr << e.what() << std::endl;
     }
 }
