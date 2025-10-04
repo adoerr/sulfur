f7d80e4f653a411d50186d019e8c4e431ee2638c
adoerr <0xad@gmx.net>
2025-10-04 09:58:22 +0200

    pipe header
    Signed-off-by: adoerr <0xad@gmx.net>

---
+++ include/libsulfur/pipe.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+
+namespace sulfur {
+    class pipe {
+    public:
+        explicit pipe(bool close_on_exec);
+        ~pipe();
+
+        [[nodiscard]] int get_read() const { return fds_[read_fd]; }
+        [[nodiscard]] int get_write() const { return fds_[write_fd]; }
+        int release_read();
+        int release_write();
+        void close_read();
+        void close_write();
+
+        std::vector<std::byte> read();
+        void write(std::byte* from, std::size_t bytes);
+
+    private:
+        static constexpr unsigned read_fd = 0;
+        static constexpr unsigned write_fd = 1;
+
+        int fds_[2]{-1, -1};
+    };
+} // namespace sulfur
