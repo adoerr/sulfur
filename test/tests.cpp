aa23d36de0affb9f9e3ed095b8fd764f871ec9cd
adoerr <0xad@gmx.net>
2025-10-09 11:21:23 +0200

    get process status
    Signed-off-by: adoerr <0xad@gmx.net>

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,6 +3,7 @@
 #include <libsulfur/error.hpp>
 #include <sys/types.h>
 #include <csignal>
+#include <fstream>
 
 using namespace sulfur;
 
@@ -10,13 +11,22 @@ namespace {
     bool process_exists(pid_t pid) {
         return kill(pid, 0) == 0 || errno != ESRCH;
     }
-}
 
-TEST_CASE("process::launch success", "[process]") {
-    const auto proc = process::launch("yes");
-    REQUIRE(process_exists(proc->pid()));
-}
+    char process_status(const pid_t pid) {
+        std::ifstream stat("/proc/" + std::to_string(pid) + "/stat");
 
-TEST_CASE("process::launch failure", "[process]") {
-    REQUIRE_THROWS_AS(process::launch("/nonexistent/path/to/executable"), error);
-}
+        std::string line;
+        std::getline(stat, line);
+        // get current process state (3rd field in /proc/[pid]/stat)
+        const size_t idx_stat = line.rfind(')') + 2;
+        return line[idx_stat];
+    }
+
+    TEST_CASE("process::launch success", "[process]") {
+        const auto proc = process::launch("yes");
+        REQUIRE(process_exists(proc->pid()));
+    }
+
+    TEST_CASE("process::launch failure", "[process]") {
+        REQUIRE_THROWS_AS(process::launch("/nonexistent/path/to/executable"), error);
+    }
